17a06579f42cf27c8b0636ac344fff3992245e42
bruno.marins <nobruin@gmail.com>
2021-03-19 17:12:33 -0300

    inclue outras pastas para a instalacao

---
+++ admin/assets/js/indisponibilidade.js
@@ -0,0 +1,3 @@
+jQuery.getScript( "https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js", function( data, textStatus, jqxhr ) {
+    jQuery('.calendar').mask('00/00/0000 00:00:00');
+  });
